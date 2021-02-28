1f98301bb9b9b9003d1cf2bf995cdf0748ab9158
Joseph Briggs <jbriggsr3@gmail.com>
2021-02-28 00:53:47 +0000

    fix(app): colors change

--- ng-challenge/src/app/components/components.module.ts
+++ ng-challenge/src/app/components/components.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core'
-import { SideMenuComponent } from './side-menu/side-menu.component'
-import { CardComponent } from './card/card.component'
-import { CommonModule } from '@angular/common'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { CommentAddComponent } from './comments/add/comment-add.component'
-import { CommentShowComponent } from './comments/show/comment-show.component'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { NgModule } from '@angular/core';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { CardComponent } from './card/card.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommentAddComponent } from './comments/add/comment-add.component';
+import { CommentShowComponent } from './comments/show/comment-show.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SearchComponent } from './search/search.component';
 
-const COMPONENTS = [CardComponent, SideMenuComponent, CommentShowComponent, CommentAddComponent]
+const COMPONENTS = [
+  CardComponent,
+  SideMenuComponent,
+  CommentShowComponent,
+  CommentAddComponent,
+  SearchComponent,
+];
 
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule, FontAwesomeModule],
